ac28be573b77a10e7670b68f5eccd555bbfb559b
Vithor Carlos <vithorcarlos@Vithors-MacBook-Air.local>
2026-01-31 15:26:22 -0300

    feat: upload arquivo controlelr

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { JwtService } from '@nestjs/jwt';
+import { StudentFactory } from 'test/factories/make-student.js';
+import { DatabaseModule } from '@/infra/database/database.module.js';
+
+describe('Upload Attachment (E2E)', () => {
+  let app: INestApplication;
+  let jwt: JwtService;
+  let studentFactory: StudentFactory;
+
+  beforeAll(async () => {
+    const { Test } = await import('@nestjs/testing');
+    const { AppModule } = await import('../../app.module.js');
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    studentFactory = moduleRef.get(StudentFactory);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test('[POST] /attachments', async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const response = await request(app.getHttpServer())
+      .post('/attachments')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .attach('file', './test/e2e/sample-upload.jpeg');
+
+    expect(response.statusCode).toBe(201);
+  });
+});
